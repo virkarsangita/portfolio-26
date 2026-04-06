1f07523f89a6041fbbacf942a9289368fc54caa7
virkar sangita <sangitavirkar2205@gmail.com>
2026-04-06 12:58:19 +0530

    fix unused imports

--- src/Commpontes/Header/Header.js
+++ src/Commpontes/Header/Header.js
@@ -1,10 +1,10 @@
 
 import React, { useState } from "react";
-import { useLocation } from "react-router-dom";
+
 import "./Header.css";
 
 function Header() {
-  const location = useLocation();
+ 
    const [darkMode, setDarkMode] = useState(false);
 
   const toggleTheme = () => {
